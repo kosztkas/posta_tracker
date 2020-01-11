e20fe1cf157a2f717e5284080500fa8d99d61d7a
Sandor Kosztka <kosztkas@users.noreply.github.com>
2020-01-11 23:24:49 +0100

    added verbose mode to see complete history

--- postatracker.py
+++ postatracker.py
@@ -11,27 +11,28 @@ def main():
     parser.add_argument('-v', '--verbose', action='store_true', help='list the detailed tracking information')
     args = parser.parse_args()
     
+    json_data = track_request(args.Number)
+    
     if args.verbose:
-        json_data = track_request(args.Number)
-        
-        #TODO
-        print "verbose mode"
+        for item in track_request_iter(json_data):
+            print('\t'.join((item.timestamp, item.location, item.status)))
     
     else:
-        json_data = track_request(args.Number)
-
-        raw_data = pyjq.all('.[] | .[0].time, .[0].postaNev, .[0].tranzakcioTipusLeirasSimple',json_data)
-        
-        result = TRACK_ENTRY(timestamp=time.ctime(raw_data[0]/1000), location=raw_data[1], status=raw_data[2])
-        
-        print('\t'.join((result.timestamp, result.location, result.status)))
-        
+        raw_data = pyjq.all('.[] | .[0].time, .[0].postaNev, .[0].tranzakcioTipusLeirasSimple',json_data)     
+        result = TRACK_ENTRY(timestamp=time.ctime(raw_data[0]/1000), location=raw_data[1], status=raw_data[2])    
+        print('\t'.join((result.timestamp, result.location, result.status)))        
         
 def track_request(number):
     url = 'https://postamobil.eu/PostaWeb/TrackingInfo?ragszam='+number+'&language=1&registered=true'
     session = requests.session()
     resp = session.get(url)
     return json.loads(resp.content)
-        
+    
+def track_request_iter(json_d):
+    i = 0
+    raw_data = pyjq.all('.[] | .[].time, .[].postaNev, .[].tranzakcioTipusLeirasSimple',json_d)
+    for i in range(len(raw_data)/3):
+        yield TRACK_ENTRY(timestamp=time.ctime(raw_data[i]/1000), location=raw_data[i+len(raw_data)/3], status=raw_data[i+2*len(raw_data)/3])
+    
 if __name__ == '__main__':
     main()
